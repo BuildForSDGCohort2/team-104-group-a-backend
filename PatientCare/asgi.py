0817739326a9a48758511ba6814c8ae9a4446478
casdon_mystery <pascalemy2010@gmail.com>
2020-08-26 11:25:12 +0100

    improvement: made some changes based on codacy recommendations

--- PatientCare/asgi.py
+++ PatientCare/asgi.py
@@ -1,8 +1,7 @@
 
-"""ASGI config for PatientCare project.
-
+"""
+ASGI config for PatientCare project.
 It exposes the ASGI callable as a module-level variable named ``application``.
-
 For more information on this file, see
 https://docs.djangoproject.com/en/3.0/howto/deployment/asgi/
 """
