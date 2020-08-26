05023d7291363b171ee1cf857eae8c87b391091d
casdon_mystery <pascalemy2010@gmail.com>
2020-08-26 11:02:07 +0100

    improvement: made some changes based on codacy recommendations

--- PatientCare/asgi.py
+++ PatientCare/asgi.py
@@ -1,3 +1,4 @@
+
 """ASGI config for PatientCare project.
 
 It exposes the ASGI callable as a module-level variable named ``application``.
@@ -5,9 +6,7 @@ It exposes the ASGI callable as a module-level variable named ``application``.
 For more information on this file, see
 https://docs.djangoproject.com/en/3.0/howto/deployment/asgi/
 """
-
 import os
-
 from django.core.asgi import get_asgi_application
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'PatientCare.settings')
--- PatientCare/wsgi.py
+++ PatientCare/wsgi.py
@@ -1,3 +1,4 @@
+
 """WSGI config for PatientCare project.
 
 It exposes the WSGI callable as a module-level variable named ``application``.
@@ -7,7 +8,6 @@ https://docs.djangoproject.com/en/3.0/howto/deployment/wsgi/
 """
 
 import os
-
 from django.core.wsgi import get_wsgi_application
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'PatientCare.settings')
