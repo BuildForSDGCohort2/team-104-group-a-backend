f5e6629e0c512252fc8c04b6daa6732367d6796b
casdon_mystery <pascalemy2010@gmail.com>
2020-09-02 10:47:57 +0100

    feat: connected the root url to the APIs urls

--- PatientCare/urls.py
+++ PatientCare/urls.py
@@ -1,9 +1,10 @@
 """PatientCare URL Configuration."""
 
 from django.contrib import admin
-from django.urls import path,include
+from django.urls import path, include
 
 urlpatterns = [
+    path('', include("backend.urls")),
     path('api/', include("backend.urls")),
     path('admin/', admin.site.urls),
 ]
