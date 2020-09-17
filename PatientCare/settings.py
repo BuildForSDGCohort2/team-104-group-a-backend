01ae13e68693b847ae973b7949eb4fa8a483cb8e
casdon_mystery <pascalemy2010@gmail.com>
2020-09-17 14:07:27 +0100

    feat: added frontend link to the white list

--- PatientCare/settings.py
+++ PatientCare/settings.py
@@ -50,7 +50,8 @@ REST_KNOX = {
     'EXPIRY_DATETIME_FORMAT': api_settings.DATETIME_FORMAT,
 }
 
-CORS_ORIGIN_WHITELIST = ["http://127.0.0.1:3000", "http://localhost:3000"]
+CORS_ORIGIN_WHITELIST = ["http://127.0.0.1:3000",
+                         "http://localhost:3000", "https://care-patient.herokuapp.com"]
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
